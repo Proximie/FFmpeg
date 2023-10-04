eaa8f1d89aa3a12cf6e488f84d412f8affc9d306
Auri Vizgaitis <auri@proximie.com>
2023-10-04 17:11:36 -0400

    Fix for ctrl-c not working when testing ffmpeg manually

--- fftools/cmdutils.c
+++ fftools/cmdutils.c
@@ -135,7 +135,8 @@ void exit_program(int ret)
     if (program_exit)
         program_exit(ret);
 
-    exit(ret);
+    if(ret!=255) /* Proximie: skip exit() so ffmpeg.exe does not hang after ctrl-c */
+        exit(ret);
 }
 
 double parse_number_or_die(const char *context, const char *numstr, int type,
